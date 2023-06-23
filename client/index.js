a9001079aa84cd30372b7cc46cb283f3e83f901a
Daniel Topping <daniel.topping@wealthwizards.com>
2023-06-23 08:33:08 +0100

    Added to the socket.io events. Syncronise the connected players with the other clients, as well as a state for interactive objects to draw on the screen later.

--- client/index.js
+++ client/index.js
@@ -1,9 +1,53 @@
 const socket = io("http://localhost:3000")
 
-socket.on("connect", () => {
-  console.log("Connected", socket.id)
+let isConnected = false
+let uids = []
+let movablesState = {}
+
+function printState() {
+  console.log("state", { isConnected, uids, movablesState })
+}
+
+socket.on("connect", (args) => {
+  console.log(
+    "%cConnected",
+    "background: green; color: lime; font-weight: 600;"
+  )
+  isConnected = true
+
+  printState()
+})
+
+socket.on("initialise", ({ movablesState: newMovablesState, connections }) => {
+  console.log("Initialised")
+  uids = connections
+  movablesState = newMovablesState
+  printState()
 })
 
 socket.on("disconnect", () => {
-  console.log("Disconnected", socket.id)
+  console.log(
+    "%cDisconnected",
+    "background: red; color: maroon; font-weight: 600;"
+  )
+  isConnected = false
+  printState()
+})
+
+socket.on("movables_state_updated", (newMovablesState) => {
+  console.log("Updated moveables")
+  movablesState = newMovablesState
+  printState()
+})
+
+socket.on("user_connected", (connectedUserId) => {
+  console.log("User connected")
+  uids = [...uids, connectedUserId]
+  printState()
+})
+
+socket.on("user_disconnected", (disconnectedUserId) => {
+  console.log("User disconnected")
+  uids = uids.filter((uid) => uid !== disconnectedUserId)
+  printState()
 })
--- server/main.js
+++ server/main.js
@@ -4,9 +4,45 @@ const io = new Server({
   cors: { origin: "*" },
 })
 
+function updateMovables() {
+  movablesState = Object.entries(movablesState).reduce((o, [name, movable]) => {
+    return { ...o, [name]: { ...movable, x: vx + x, y: vy + y } }
+  }, movablesState)
+
+  io.emit("movables_state_updated", movablesState)
+}
+
+setInterval(updateMovables, 1000)
+
+const connections = []
+let movablesState = { redBox: { x: 0, y: 0, vx: 0.1, vy: 0.5 } }
+
+function registerSocketListeners(socket) {
+  socket.on("update_moveables", ({ movableId, position }) => {
+    movablesState = { ...movablesState, [movableId]: position }
+    socket.broadcast.emit("movables_state_updated", movablesState)
+  })
+
+  socket.on("disconnect", () => {
+    const indexToDelete = connections.indexOf(socket.id)
+    socket.broadcast.emit("user_disconnected", socket.id)
+    connections.splice(indexToDelete, 1)
+    console.log(
+      `Disconnect: ${socket.id}\nActive connections: [${connections.join(",")}]`
+    )
+  })
+}
+
 io.on("connection", (socket) => {
-  socket.emit("connected")
-  console.log("New connection")
+  socket.emit("initialise", { movablesState, connections })
+  socket.broadcast.emit("user_connected", socket.id)
+  connections.push(socket.id)
+  console.log(
+    `New connection: ${socket.id}\nActive connections: [${connections.join(
+      ","
+    )}]`
+  )
+  registerSocketListeners(socket)
 })
 
 io.listen(3000)
